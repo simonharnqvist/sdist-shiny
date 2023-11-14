248b1878ff2e89dcdf819a817d62929f2b3f6b47
Simon Harnqvist <simon.harnqvist@ed.ac.uk>
2023-11-14 13:35:51 +0000

    removed redundant files

--- build/lib/sdist_shiny/run_app.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-from pathlib import Path
-
-def main():
-    dir = Path(__file__).resolve().parent
-    subprocess.run(["shiny", "run", "--reload", "-b", f"{str(dir)}/app.py"], check=True)
-
-if __name__ == "__main__":
-    main()
